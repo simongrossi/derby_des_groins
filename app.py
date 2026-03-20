d026312d64a29fb326377e573116e8208e5938f6
Simon Grossi <simon.grossi@gmail.com>
2026-03-20 20:15:52 +0100

    chore: change default port to 5001 to avoid AirPlay conflict on macOS

--- app.py
+++ app.py
@@ -204,4 +204,5 @@ def ensure_balance_transaction_snapshots():
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(debug=True, host='0.0.0.0', port=port)
