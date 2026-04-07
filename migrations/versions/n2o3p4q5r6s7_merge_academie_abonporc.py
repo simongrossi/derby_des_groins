50c2a46491d2033fc3d4da6df3197a7ef3efacb8
Simon Grossi <simon.grossi@gmail.com>
2026-04-07 17:12:51 +0200

    fix(migrations): merge academie and abonporc heads

---
+++ migrations/versions/n2o3p4q5r6s7_merge_academie_abonporc.py
@@ -0,0 +1,22 @@
+"""merge academie and abonporc heads
+
+Revision ID: n2o3p4q5r6s7
+Revises: e7563f9fde8c, m1n2o3p4q5r6
+Create Date: 2026-04-07 18:00:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+revision = 'n2o3p4q5r6s7'
+down_revision = ('e7563f9fde8c', 'm1n2o3p4q5r6')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
